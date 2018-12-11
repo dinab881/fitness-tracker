90dd996470b9f659a1e3988759edaa7535d26f87
Dina <dina.bbb88@gmail.com>
2018-12-12 00:27:03 +0200

    Initial commit

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+import {MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule} from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  declarations: []
+})
+export class MaterialModule { }
